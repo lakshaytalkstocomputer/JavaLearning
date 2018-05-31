d205b46c4b0bbb3be4b418cd2c5df7ea1bbbdeb2
Lakshay Sagar Rana <38259381+lakshaytalkstocomputer@users.noreply.github.com>
2018-06-01 00:54:08 +0530

    Update Extract.java

--- Beginner/Extract.java
+++ Beginner/Extract.java
@@ -7,7 +7,7 @@ class Extract
 		System.out.println("Enter string are :");
 		boolean space = false;
 		String s1=sc.nextLine();
-		
+		/*
 		for(int i=0;i<s1.length();i++)
 		{
 			System.out.println("CHAR AT "+i+" : "+s1.charAt(i));
@@ -27,7 +27,8 @@ class Extract
 				System.out.println(s1);
 			}	
 			
-		}
+		} */
+		s1 = s1.replaceAll("[^0-9]", "");
 		System.out.println("New String = " +s1);
 	}
 }
