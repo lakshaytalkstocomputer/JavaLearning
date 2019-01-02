9bd3118846544f022919ed79426737cff4d5768f
Lakshay <lakshaysagarrana@gmail.com>
2019-01-02 10:55:37 +0530

    Demo of Unsynchornized

---
+++ ThreadingAndNetworking/src/threadingandnetworking/UnSynchronized.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadingandnetworking;
+
+/**
+ *
+ * @author silverpanda
+ */
+class Callme
+{
+    void call(String msg)
+    {
+        System.out.println("[" + msg);
+    
+    try
+    {
+        Thread.sleep(1000);
+    }
+    catch(InterruptedException e)
+    {
+        System.out.println("Interrupted");
+    }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable
+{
+    String msg;
+    Thread t;
+    Callme target;
+    public Caller(Callme targ, String s)
+    {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+    }
+    
+    public void run()
+    {
+        target.call(msg);
+    }
+    
+        }
+
+public class UnSynchronized {
+    public static void main(String args[])
+    {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Hello");
+        Caller ob2 = new Caller(target, "Synchronised");
+        Caller ob3 = new Caller(target, "World");
+        
+        ob1.t.start();
+        ob2.t.start();
+        ob3.t.start();
+        
+        // wait for thread to end
+        try 
+        {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        }
+        catch(InterruptedException e)
+            
+        {
+            System.out.println("Interrupted");
+        }
+    }
+}
